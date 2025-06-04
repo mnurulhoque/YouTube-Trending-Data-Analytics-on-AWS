aeeed861d685b8cbf78a1409c7ecf1484be2f1d1
Md Nurul Hoque <152673435+mnurulhoque@users.noreply.github.com>
2025-06-04 02:22:25 -0400

    Update PySpark (ETL) scripts-parquet-analytics-version.py

--- PySpark (ETL) scripts-parquet-analytics-version.py
+++ PySpark (ETL) scripts-parquet-analytics-version.py
@@ -4,54 +4,35 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from awsgluedq.transforms import EvaluateDataQuality
 
-############################### Added by Carlos ###############################
-from awsglue.dynamicframe import DynamicFrame
-
-
-## @params: [JOB_NAME]
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
-
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
-## @type: DataSource
-## @args: [database = "db_youtube_raw", table_name = "raw_statistics", transformation_ctx = "datasource0"]
-## @return: datasource0
-## @inputs: []
-############################### Added by Darshil ###############################
-predicate_pushdown = "region in ('ca','gb','us')"
 
-datasource0 = glueContext.create_dynamic_frame.from_catalog(database = "db_youtube_raw", table_name = "raw_statistics", transformation_ctx = "datasource0", push_down_predicate = predicate_pushdown)
+# Default ruleset used by all target nodes with data quality enabled
+DEFAULT_DATA_QUALITY_RULESET = """
+    Rules = [
+        ColumnCount > 0
+    ]
+"""
 
-## @type: ApplyMapping
-## @args: [mapping = [("video_id", "string", "video_id", "string"), ("trending_date", "string", "trending_date", "string"), ("title", "string", "title", "string"), ("channel_title", "string", "channel_title", "string"), ("category_id", "long", "category_id", "long"), ("publish_time", "string", "publish_time", "string"), ("tags", "string", "tags", "string"), ("views", "long", "views", "long"), ("likes", "long", "likes", "long"), ("dislikes", "long", "dislikes", "long"), ("comment_count", "long", "comment_count", "long"), ("thumbnail_link", "string", "thumbnail_link", "string"), ("comments_disabled", "boolean", "comments_disabled", "boolean"), ("ratings_disabled", "boolean", "ratings_disabled", "boolean"), ("video_error_or_removed", "boolean", "video_error_or_removed", "boolean"), ("description", "string", "description", "string"), ("region", "string", "region", "string")], transformation_ctx = "applymapping1"]
-## @return: applymapping1
-## @inputs: [frame = datasource0]
-applymapping1 = ApplyMapping.apply(frame = datasource0, mappings = [("video_id", "string", "video_id", "string"), ("trending_date", "string", "trending_date", "string"), ("title", "string", "title", "string"), ("channel_title", "string", "channel_title", "string"), ("category_id", "long", "category_id", "long"), ("publish_time", "string", "publish_time", "string"), ("tags", "string", "tags", "string"), ("views", "long", "views", "long"), ("likes", "long", "likes", "long"), ("dislikes", "long", "dislikes", "long"), ("comment_count", "long", "comment_count", "long"), ("thumbnail_link", "string", "thumbnail_link", "string"), ("comments_disabled", "boolean", "comments_disabled", "boolean"), ("ratings_disabled", "boolean", "ratings_disabled", "boolean"), ("video_error_or_removed", "boolean", "video_error_or_removed", "boolean"), ("description", "string", "description", "string"), ("region", "string", "region", "string")], transformation_ctx = "applymapping1")
-## @type: ResolveChoice
-## @args: [choice = "make_struct", transformation_ctx = "resolvechoice2"]
-## @return: resolvechoice2
-## @inputs: [frame = applymapping1]
-resolvechoice2 = ResolveChoice.apply(frame = applymapping1, choice = "make_struct", transformation_ctx = "resolvechoice2")
-## @type: DropNullFields
-## @args: [transformation_ctx = "dropnullfields3"]
-## @return: dropnullfields3
-## @inputs: [frame = resolvechoice2]
-dropnullfields3 = DropNullFields.apply(frame = resolvechoice2, transformation_ctx = "dropnullfields3")
-## @type: DataSink
-## @args: [connection_type = "s3", connection_options = {"path": "s3://bigdata-on-youtube-cleansed-euwest1-14317621-dev/youtube/raw_statistics/"}, format = "parquet", transformation_ctx = "datasink4"]
-## @return: datasink4
-## @inputs: [frame = dropnullfields3]
+# Script generated for node AWS Glue Data Catalog
+AWSGlueDataCatalog_node1746854753443 = glueContext.create_dynamic_frame.from_catalog(database="db_youtube_cleaned", table_name="raw_statistics", transformation_ctx="AWSGlueDataCatalog_node1746854753443")
 
-############################### Added by Darshil ###############################
-#MAKE SURE YOU COPY ONLY WHAT IS NEEDED
+# Script generated for node AWS Glue Data Catalog
+AWSGlueDataCatalog_node1746854564565 = glueContext.create_dynamic_frame.from_catalog(database="db_youtube_cleaned", table_name="cleaned_statistics_reference_data", transformation_ctx="AWSGlueDataCatalog_node1746854564565")
 
-datasink1 = dropnullfields3.toDF().coalesce(1)
-df_final_output = DynamicFrame.fromDF(datasink1, glueContext, "df_final_output")
-datasink4 = glueContext.write_dynamic_frame.from_options(frame = df_final_output, connection_type = "s3", connection_options = {"path": "s3://de-on-youtube-cleansed-useast1-dev/youtube/raw_statistics/", "partitionKeys": ["region"]}, format = "parquet", transformation_ctx = "datasink4")
+# Script generated for node Join
+Join_node1746854802917 = Join.apply(frame1=AWSGlueDataCatalog_node1746854753443, frame2=AWSGlueDataCatalog_node1746854564565, keys1=["category_id"], keys2=["id"], transformation_ctx="Join_node1746854802917")
 
-############################### Added by Darshil ###############################
+# Script generated for node Amazon S3
+EvaluateDataQuality().process_rows(frame=Join_node1746854802917, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1746854278844", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
+AmazonS3_node1746856147080 = glueContext.getSink(path="s3://ds-youtube-analytics-uswest2-dev", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=["region", "category_id"], enableUpdateCatalog=True, transformation_ctx="AmazonS3_node1746856147080")
+AmazonS3_node1746856147080.setCatalogInfo(catalogDatabase="db_youtube_analytics",catalogTableName="final_analytics")
+AmazonS3_node1746856147080.setFormat("glueparquet", compression="snappy")
+AmazonS3_node1746856147080.writeFrame(Join_node1746854802917)
 job.commit()
